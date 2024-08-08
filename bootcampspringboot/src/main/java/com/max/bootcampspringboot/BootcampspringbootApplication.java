54f63b8dc739fc6542d0960c37a4a022b2122a8d
Max Grimm <max.grimm@stud.h-da.de>
2024-08-08 15:45:49 +0200

    intializr

---
+++ bootcampspringboot/src/main/java/com/max/bootcampspringboot/BootcampspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.max.bootcampspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampspringbootApplication.class, args);
+	}
+
+}
---
+++ bootcampspringboot/src/test/java/com/max/bootcampspringboot/BootcampspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.max.bootcampspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
